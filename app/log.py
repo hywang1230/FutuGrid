6cd4806f8d5438e00d2e7282c3cc7c62a64a8e70
wangrong <hywangrong@gmail.com>
2022-07-27 22:49:28 +0800

    first commit

--- app/log.py
+++ app/log.py
@@ -1,3 +1,3 @@
 import logging
 
-logging.basicConfig(level=logging.INFO, filename='../app.log', filemode='a+', format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, filename='./app.log', filemode='a+', format='%(asctime)s - %(levelname)s - %(message)s')
